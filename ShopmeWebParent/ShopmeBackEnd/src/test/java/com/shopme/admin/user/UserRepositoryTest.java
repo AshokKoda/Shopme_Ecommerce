e416d3d477f945842d167ddf2add9645bf82ab70
Koda Ashok <a.koda59@gmail.com>
2022-07-21 16:39:59 +0530

    Changes is done

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository repo;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateNewUserWithOneRole() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User user = new User("a.koda59@gmail.com", "0000", "Koda", "Ashok");
+		user.addRole(roleAdmin);
+
+		User savedUser = repo.save(user);
+
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateNewUserWithTwoRole() {
+		User user = new User("ravi@gmail.com", "0000", "Ravi", "Kumar");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+
+		user.addRole(roleEditor);
+		user.addRole(roleAssistant);
+
+		User savedUser = repo.save(user);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+	}
+
+	@Test
+	public void testGetUserById() {
+		User user = repo.findById(1).get();
+		System.out.println("User is : " + user);
+		assertThat(user).isNotNull();
+	}
+	
+	@Test
+	public void testUpdateUserDetails() {
+		User user = repo.findById(1).get();
+		user.setEnabled(true);
+		user.setEmail("ashok@gmail.com");
+		System.out.println("User is updated success.");
+		repo.save(user);
+	}
+	
+	@Test
+	public void testUpdateUserRoles() {
+		User user = repo.findById(2).get();
+		
+		Role editorRole = new Role(3);
+		user.getRoles().remove(editorRole);
+		
+		Role salePerson = new Role(2);
+		user.addRole(salePerson);
+		
+		repo.save(user);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		Integer userId = 2;
+		repo.deleteById(userId);
+	}
+}
